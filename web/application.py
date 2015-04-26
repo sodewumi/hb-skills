d949390f5b3981341c267ef8b29b7b329e064117
Hackbright Student <info@hackbrightacademy.com>
2015-04-25 22:07:37 -0700

    Bug in flask application. webpage won't render

--- web/application.py
+++ web/application.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index_page():
     return render_template("index.html")
 
+@app.route("/response")
+def write_response():
+    last_name_py = request.args.get("last_name_form")
+    first_name_py = request.args.get("first_name_form")
+    salary_py = request.args.get("salary_form")
+    print last_name_py
+    print first_name_py
+    print salary_py
+
+    return render_template("response.html", first_name_response =first_name_py,
+        last_name_response = last_name_py, salary_response = salary_py)
+
 if __name__ == "__main__":
     app.run(debug=True)

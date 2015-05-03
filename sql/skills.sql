9046b36e9d8bb4af992d9f091139329503b5469e
Hackbright Student <info@hackbrightacademy.com>
2015-05-02 17:54:57 -0700

    Finished skills. Started first question of further study

--- sql/skills.sql
+++ sql/skills.sql
@@ -47,10 +47,10 @@ OFFSET 15;
 --    (The year the brand was founded should be ``null`` if 
 --    the brand is not in the Brands table.)
 
-SELECT m.brand_name, b.name, m.name, b.founded FROM Models AS m
+SELECT m.brand_name, b.name, m.name, b.founded, m.year FROM Models AS m
 LEFT JOIN Brands AS b
 ON m.brand_name = b.name
-WHERE m.year >= 1960;
+WHERE m.year = 1960;
 
 
 
@@ -60,23 +60,23 @@ WHERE m.year >= 1960;
 
 -- 1. Modify this query so it shows all **brands** that are not discontinued
 -- regardless of whether they have any cars in the cars table.
--- before:
-    -- SELECT b.name,
-    --        b.founded,
-    --        m.name
-    -- FROM Model AS m
-    --   LEFT JOIN brands AS b
-    --     ON b.name = m.brand_name
-    -- WHERE b.discontinued IS NULL;
+-- before:F
+    SELECT b.name,
+           b.founded,
+           m.name
+    FROM Brands AS b
+      LEFT JOIN Models AS m
+        ON b.name = m.brand_name
+    WHERE b.discontinued IS NULL;
 
 -- 2. Modify this left join so it only selects models that have brands in the Brands table.
 -- before: 
-    -- SELECT m.name,
-    --        m.brand_name,
-    --        b.founded
-    -- FROM Models AS m
-    --   LEFT JOIN Brands AS b
-    --     ON b.name = m.brand_name;
+    SELECT m.name,
+           m.brand_name,
+           b.founded
+    FROM Models AS m
+      INNER JOIN Brands AS b
+        ON b.name = m.brand_name;
 
 -- followup question: In your own words, describe the difference between 
 -- left joins and inner joins.
@@ -84,25 +84,26 @@ WHERE m.year >= 1960;
 -- 3. Modify the query so that it only selects brands that don't have any car models in the cars table. 
 -- (Hint: it should only show Tesla's row.)
 -- before: 
-    -- SELECT name,
-    --        founded
-    -- FROM Brands
-    --   LEFT JOIN Models
-    --     ON brands.name = Models.brand_name
-    -- WHERE Models.year > 1940;
+    SELECT brands.name,
+           brands.founded
+    FROM Brands
+      LEFT JOIN Models
+        ON brands.name = Models.brand_name
+    WHERE Models.brand_name IS NULL;
 
 -- 4. Modify the query to add another column to the results to show 
 -- the number of years from the year of the model *until* the brand becomes discontinued
 -- Display this column with the name years_until_brand_discontinued.
 -- before: 
-    -- SELECT b.name,
-    --        m.name,
-    --        m.year,
-    --        b.discontinued
-    -- FROM Models AS m
-    --   LEFT JOIN brands AS b
-    --     ON m.brand_name = b.name
-    -- WHERE b.discontinued NOT NULL;
+    SELECT b.name,
+           m.name,
+           m.year,
+           b.discontinued,
+           (b.discontinued - m.year) AS years_until_brand_discontinued
+    FROM Models AS m
+      LEFT JOIN brands AS b
+        ON m.brand_name = b.name
+    WHERE b.discontinued NOT NULL;
 
 
 
